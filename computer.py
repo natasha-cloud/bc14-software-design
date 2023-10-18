4ba25a311cd3feebe8a187f6fa6bc75ca82bad8d
senjack <senjackglobal@gmail.com>
2023-10-18 14:57:30 +0300

    Add changes

--- computer.py
+++ computer.py
@@ -33,10 +33,28 @@ class Computer():
     def input(self):
         return self.__inputDevice.input()
 
+    # Setter for input device
+    def set_input_device(self, input_device):
+        self.__input_device = input_device
 
-class Desktop(Computer):
-    pass
+    # Getter for input device
+    def get_input_device(self):
+        return self.__input_device
 
-computer1 = Computer(Mouse())
-computer1.input()
+# Computer Objects
+computer1:Computer = Computer(Keyboard())
 
+# Change input device dynamically using a setter
+computer1.set_input_device(Mouse())
+computer1.set_input_device(Keyboard())
+
+# Access the value of a private field using a getter
+computer1.set_input_device(Keyboard())
+print(computer1.get_input_device())
+
+def test_computer(computer):
+    computer.input()
+    # computer.retrieve("name")
+    # computer.output(computer.category)
+
+test_computer(computer1)
